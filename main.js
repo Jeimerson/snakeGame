6c24a65046da50c14de35cf0136a74f9f2808b59
alanams97 <alan_ams97@gmail.com>
2020-03-18 23:19:14 -0300

    Styles

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ function startGame() {
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(game);
             alert('Game Over :(')
+            alert('Para recomeçar o jogo, recarregue a página :D')
         }
     }
 
